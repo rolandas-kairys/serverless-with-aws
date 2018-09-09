0588f073f6b279b6713a9f12996e9e2fcd77b39f
rolandas-kairys <rolandas.kairys@gmail.com>
2018-09-09 22:43:57 +0300

    not using profile

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,8 +1,12 @@
 module.exports.handler = (event, ctx, done) => {
+  console.log(JSON.stringify(ctx, null, 2));
+
   done(null, {
     statusCode: 200,
-    body: JSON.stringify({data: {
-      id: 1, name: 'clean up', status: 'open'
-    }})
+    body: JSON.stringify({
+      data: {
+        id: 1, name: 'clean up', status: 'open'
+      }
+    })
   })
 }
