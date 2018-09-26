07fcc12a758f72dca38b608d82cbda0faa2bd4c5
rolandas-kairys <rolandas.kairys@gmail.com>
2018-09-26 11:18:54 +0300

    testing todos on a supertest

--- src/api/api.spec.js
+++ src/api/api.spec.js
@@ -1,6 +1,18 @@
+const app = require('./api')
+const supertest = require('supertest')
 
 describe('API', () => {
-  test('should be ok', () => {
-    expect(1).toBe(1)
+  let request
+  beforeEach(() => {
+    request = supertest(app)
+  })
+
+  test('should get todos', done => {
+    request
+      .get('/todos')
+      .expect(200, (err, res) => {
+        expect(res.body.data).toHaveLength(2)
+        done()
+      })
   })
 })
